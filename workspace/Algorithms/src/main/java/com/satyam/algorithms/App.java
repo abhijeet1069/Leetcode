80da36d292f007b5c5e962c247a0dc5168e5c64a
abhijeet1069 <abhijeet1069@gmail.com>
2025-10-11 15:23:02 +0530

    logging started working using log4j2

--- workspace/Algorithms/src/main/java/com/satyam/algorithms/App.java
+++ workspace/Algorithms/src/main/java/com/satyam/algorithms/App.java
@@ -1,13 +1,15 @@
 package com.satyam.algorithms;
 
-/**
- * Hello world!
- *
- */
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class App 
 {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	logger.info("App started");
+    	logger.info("App started");
     }
 }
