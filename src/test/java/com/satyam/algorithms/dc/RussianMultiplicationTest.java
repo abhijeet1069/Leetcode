8d640b4746017ee158835423805c7304a2201022
abhijeet1069 <abhijeet1069@gmail.com>
2025-12-07 13:10:32 +0530

    practise monotonic stack questions in office time also

---
+++ src/test/java/com/satyam/algorithms/dc/RussianMultiplicationTest.java
@@ -0,0 +1,16 @@
+package com.satyam.algorithms.dc;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RussianMultiplicationTest {
+
+    @Test
+    void multiply() {
+        RussianMultiplication mul = new RussianMultiplication();
+        assertEquals(15, mul.multiply(5,3));
+        assertEquals(15, mul.multiply(3,5));
+        assertEquals(1012, mul.multiply(23,44));
+    }
+}
